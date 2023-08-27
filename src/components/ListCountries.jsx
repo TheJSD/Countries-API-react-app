6cce5606f26add8e5e81689a47aafef5be04a268
James Dickson <jamesstuartdickson@gmail.com>
2023-08-27 19:30:03 +0100

    deleted some leftover code that was commented out

--- src/components/ListCountries.jsx
+++ src/components/ListCountries.jsx
@@ -1,22 +1,6 @@
 
 import React from 'react'
 
-
-// const countryItems = function () {
-//   const countryJSXItems = []
-//   for (const country in allCountries) {
-//     countryJSX = (
-//     <li key={country.cca3}><button onClick={selectCountry} value={country.cca3}>
-//     {country.name.official}
-//     </button>
-//     <p>Population: {country.population}</p>
-//     </li>
-//     )
-//     countryJSXItems.push(countryJSX)
-//   }
-//   return countryJSXItems
-// }
-
 const ListCountries = ({allCountries, selectCountry, countryItems}) => {
   
   return (
@@ -37,25 +21,3 @@ const ListCountries = ({allCountries, selectCountry, countryItems}) => {
 }
 
 export default ListCountries
-
-// old code
-// const ListCountries = ({allCountries, selectCountry}) => {
-//   return (
-//     <>
-
-    
-//     <h1>List of Countries:</h1>
-//       <ul>
-      // {allCountries.map((country, index) => {
-      // return (
-      //   <li key={index}><button onClick={selectCountry} value={index}>
-      //   {country.name.official}
-      //   </button>
-      //   <p>Population: {country.population}</p>
-      //   </li>
-//       )
-//     })}
-//   </ul>
-//   </>
-//   )
-// }
